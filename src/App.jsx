e5ba8178e5d7913fb90582b7e4c53ac2674d83e1
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-05-28 15:45:17 -0300

    substituindo o index.css por um styled global

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { GlobalStyled } from './GlobalStyled'
 import Home from './pages/home/Home'
+
 
 function App() {
   return (
-    <Home />
+    <>
+      <GlobalStyled/>  
+      <Home />
+    </>
   )
 }
 
